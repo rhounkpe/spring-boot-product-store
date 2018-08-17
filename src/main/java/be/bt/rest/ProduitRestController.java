493d2e57dd2117103909b5783b9ed6b517c2637a
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2018-08-17 12:53:52 +0200

    GetProduct by id supported on server side

--- src/main/java/be/bt/rest/ProduitRestController.java
+++ src/main/java/be/bt/rest/ProduitRestController.java
@@ -1,10 +1,14 @@
 package be.bt.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,14 @@ public class ProduitRestController {
 	@GetMapping("")
 	public List<Produit> getAll() {
 		return repo.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produit> getProduitById(@PathVariable String id) {
+		Optional<Produit> result = repo.findById(id);
+		
+		return (result.isPresent())? new ResponseEntity<>(result.get(), HttpStatus.OK)
+				:
+					new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 }
